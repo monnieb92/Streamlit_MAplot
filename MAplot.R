aa94dda126a26fe3bb70449bdb7670caee73676d
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 18:41:45 -0500

    Update MAplot.R

--- MAplot.R
+++ MAplot.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 
+result_table_filtered <- read.csv("result_table_filtered.csv", sep = " ")
+
 theme_monica <- function(){
   theme_classic() %+replace%    #replace elements we want to change
     #font <- "Times", 
