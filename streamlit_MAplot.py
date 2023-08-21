77cbe818a7fa5c87abd10ebd771170d427a26e52
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 14:26:55 -0500

    Update streamlit_MAplot.py

--- streamlit_MAplot.py
+++ streamlit_MAplot.py
@@ -104,7 +104,7 @@ ggsave("MAplot.png", plot = MAplotoutput ,width = 4.25, height = 3, dpi = 300)
 st.code(code1, language='R')
 process2 = subprocess.Popen(["Rscript", "plot.R", str(uploaded_file),str(adjp), str(foldchangeup), str(foldchangedn)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result2 = process2.communicate()
-count_result
+# Extract count results 
 count_result_lines = result2.split('\n')
 for line in count_result_lines:
     if "count_result" in line:
