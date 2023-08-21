de73faf9625f76a6361ab290bc592581d44b8509
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 14:45:08 -0500

    Update MAplot.R

--- MAplot.R
+++ MAplot.R
@@ -1,12 +1,12 @@
 library(ggplot2)
-library(dplyr)
 library(tidyverse)
-  
+library(dplyr)
+
 result_table <- read_tsv(uploaded_file)
 result_table[is.na(result_table)] <- 1
 
 result_table_filtered <- result_table %>% 
-  mutate(FoldChange = ((2^(abs(log2FoldChange)))*sign(log2FoldChange))) %>% 
+  dplyr::mutate(FoldChange = ((2^(abs(log2FoldChange)))*sign(log2FoldChange))) %>% 
   transform( category = ifelse((padj <= adjp &  FoldChange <= -foldchangedn), "Down", ifelse((padj <= adjp &  FoldChange >= foldchangeup), "Up",  "NS")))
 
 count_result <- dplyr::count(result_table_filtered, category)
