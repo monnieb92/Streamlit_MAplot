a0f7ca77b48d1e707994f1ff07eb20e9917fe6f7
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 18:21:37 -0500

    Add files via upload

---
+++ DifferentialTable.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+result_table <- upload_file_df
+result_table[is.na(result_table)] <- 1
+
+result_table_filtered <- result_table %>% 
+  mutate(FoldChange = ((2^(abs(log2FoldChange)))*sign(log2FoldChange))) %>% 
+  transform( category = ifelse((padj <= adjp &  FoldChange <= -foldchangedn), "Down", ifelse((padj <= adjp &  FoldChange >= foldchangeup), "Up",  "NS")))
+
+count_result <- dplyr::count(result_table_filtered, category)
