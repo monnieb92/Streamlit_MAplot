81308ffe3249d09a9a4de5ba763fc5f3917c2252
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 17:46:38 -0500

    Update streamlit_MAplot.py

--- streamlit_MAplot.py
+++ streamlit_MAplot.py
@@ -107,9 +107,13 @@ ggsave("MAplot.png", plot = MAplotoutput ,width = 4.25, height = 3, dpi = 300)
 
 st.code(code1, language='R')
 
+uploaded_file_bytes = None  # Initialize the variable outside the if block
+
 if uploaded_file is not None: 
-    # convert to BytesIO object
+    # Convert to BytesIO object
     uploaded_file_bytes = BytesIO(uploaded_file.read())
+
+# Rest of your code
 
 process2 = subprocess.Popen(["Rscript", "MAplot.R",str(adjp), str(foldchangeup), str(foldchangedn)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result2, error2 = process2.communicate(input=uploaded_file_bytes.read().decode())
