e797e66dfffc15205e6230a191b6892a23aa02f9
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 23:32:01 -0500

    Update streamlit_MAplot.py

--- streamlit_MAplot.py
+++ streamlit_MAplot.py
@@ -54,7 +54,7 @@ dplyr::count(result_table_filtered, category)
 '''
 st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "DifferentialTable.R", str(adjp), str(foldchangeup), str(foldchangedn),
-    uploaded_file.name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    uploaded_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1, error1 = process1.communicate()
 if process1.returncode == 0: 
     filtered_result = result_table_filtered.copy()
