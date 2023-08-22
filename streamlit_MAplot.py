767d114931bdeb4c7da5bbdefd881d97f7f6e5d8
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-21 19:24:52 -0500

    Update streamlit_MAplot.py

--- streamlit_MAplot.py
+++ streamlit_MAplot.py
@@ -49,7 +49,7 @@ result_table_filtered <- result_table %>%
   mutate(FoldChange = ((2^(abs(log2FoldChange)))*sign(log2FoldChange))) %>% 
   transform( category = ifelse((padj <= adjp &  FoldChange <= -foldchangedn), "Down", ifelse((padj <= adjp &  FoldChange >= foldchangeup), "Up",  "NS")))
 
-count_result <- dplyr::count(result_table_filtered, category)
+dplyr::count(result_table_filtered, category)
 '''
 st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "DifferentialTable.R", str(adjp), str(foldchangeup), str(foldchangedn)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
