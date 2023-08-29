9c9a0fffc8344fbf2e3992a5d556ee200dc00d88
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-29 16:44:57 -0500

    Update MAplot_Streamlit_app.py

--- MAplot_Streamlit_app.py
+++ MAplot_Streamlit_app.py
@@ -47,8 +47,8 @@ if st.button('Determine Differential analysis'):
         )
 
 # Button click to create MA plot
-if st.button('Create MA plot')
-    if 'result_table_filtered' not in locals():
+if st.button('Create MA plot'):
+    if 'result_table' not in locals():
         st.write("Please perform the differential analysis first.")
     else:
         # Create the MA plot using Plotly
