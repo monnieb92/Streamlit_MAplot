9ed8aff086415934993b924ba44c6040683cbfb0
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-30 13:23:36 -0500

    Update MAplot_Streamlit_app.py

--- MAplot_Streamlit_app.py
+++ MAplot_Streamlit_app.py
@@ -26,7 +26,7 @@ padj_column=st.text_area('The name of the column containing the adjusted p-value
 
 l2FChg_column=st.text_area('The name of the column containing the log2fold Change values',value='log2FoldChange')
 maplot_title=st.text_area('The title of the MA plot',value='Differential Analysis')
-titlesize_font.number_input('The font size of the Title',value=15)
+titlesize_font=st.number_input('The font size of the Title',value=15)
 yax_min=st.number_input('The minimun value of yaxis',value=-8)
 yax_max=st.number_input('The maximum value of yaxis',value=8)
 yax_font=st.number_input('The y-axis title font size',value=12)
