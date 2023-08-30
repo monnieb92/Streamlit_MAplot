b42df21795c6d32d5e6c05b16852d94b17e9ed1e
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-30 13:47:36 -0500

    Update MAplot_Streamlit_app.py

--- MAplot_Streamlit_app.py
+++ MAplot_Streamlit_app.py
@@ -74,7 +74,7 @@ if st.button('Determine Differential analysis'):
      # Add horizontal line at y = 0
     fig.add_shape(  go.layout.Shape(type="line",
          xref="paper"
-         x0=0.01,
+         x0=0,
          x1=1,
          y0=0,
          y1=0,
