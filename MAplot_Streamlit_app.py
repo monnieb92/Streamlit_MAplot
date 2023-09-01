7d95b9bb8127bf87a63011275fb90f6f204b4369
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-09-01 11:07:52 -0500

    Update MAplot_Streamlit_app.py

--- MAplot_Streamlit_app.py
+++ MAplot_Streamlit_app.py
@@ -90,7 +90,7 @@ if st.button('Determine Differential analysis'):
     fig.update_yaxes(ticks="outside", tickvals=list(range(yax_min, (yax_max+1),2)), range=[yax_min, yax_max],dtick=2,showgrid=False)
 
         # Set x-axis ticks for log scale
-    fig.update_xaxes(ticks="outside", tickvals=[10, 100, 1000, 10000],showgrid=False)
+   # fig.update_xaxes(ticks="outside", tickvals=[10, 100, 1000, 10000],showgrid=False)
 
         # Update layout settings
     fig.update_layout(showlegend=True,
