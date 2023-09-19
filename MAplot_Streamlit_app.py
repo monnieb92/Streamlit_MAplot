f6866447ddec60d95ad4c79dd2f20f00646585e4
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-09-19 11:15:07 -0500

    Update MAplot_Streamlit_app.py

--- MAplot_Streamlit_app.py
+++ MAplot_Streamlit_app.py
@@ -71,7 +71,7 @@ if st.button('Determine Differential analysis'):
         x='baseMean',
         y='log2FoldChange',
         color='category',
-        text='dot_names',
+        text=dot_names,
         color_discrete_map={'Up': '#ca0020', 'NS': '#bababa', 'Down': '#0571b0'},
         opacity=1,
         title=maplot_title)
