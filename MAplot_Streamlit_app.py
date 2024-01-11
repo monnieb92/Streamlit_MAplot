9adee898ff98bfd2c554fcd8783b3572b1a5120f
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2024-01-11 15:15:15 -0600

    Update MAplot_Streamlit_app.py

--- MAplot_Streamlit_app.py
+++ MAplot_Streamlit_app.py
@@ -39,6 +39,8 @@ sizeofmarker=st.number_input('The dot size',value=2)
 figure_width=st.number_input('The width of the figure',value=500)
 figure_height=st.number_input('The height of the figure',value=500)
 xax_tick_range_max=st.number_input('The x-axis tick range max number if not log transforming baseMean',value=6)
+st.write('The 6 tick values added when not log transforming baseMean are:
+tickvals=[0, 1, 10, 100, 1000, 10000,100000,1000000]')
 fontnames=st.text_area('The name of the fonts, Arial or Arial Black',value='Arial Black')
 logchoice=st.selectbox("Pick a choice for the transformation of the baseMean", ["log2", "log10", "None"])
 # Correct GitHub raw CSV file URL
